d9d5d7f3ed329aeef8ca95c04848a94b30dde6b2
Deno Norsanto <deno.nors@gmail.com>
2022-06-10 10:35:27 +0700

    Update fungsiCustom.js

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,26 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const olahData = (rawData) => {
+  if (rawData.message !== undefined) {
+    return rawData.message.split(" ")[1];
+  }
+  else if (rawData[0].message !== undefined) {
+    return rawData[0].message.split(" ")[1];
+  }
+  else if (rawData[0].data.message !== undefined) {
+    return rawData[0].data.message.split(" ")[1];
+  }
+};
+const bacaData = async(callback) => {
+  let arrayFile = [file1, file2, file3];
+  let data = [];
+    for (const element of arrayFile) {
+      data.push(olahData(JSON.parse(await fs.promises.readFile(element))));
+    }
+    callback(null, data);
+};
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
